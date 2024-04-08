e48e841d0bccda3ee4de31edc78013bb31230f97
phuocloi0502 <birdverybig103@gmail.com>
2024-04-08 22:23:12 +0900

    ok

--- React JS/Buoi 5/Raiway81_VoPhuocLoi_Buoi5_State/src/components/Footer.jsx
+++ React JS/Buoi 5/Raiway81_VoPhuocLoi_Buoi5_State/src/components/Footer.jsx
@@ -1,6 +1,30 @@
 import '../css/footer.css'
+import { useState } from 'react';
 export const Footer = () => {
-    return (
-        <div className="footer">Compare plans </div>
-    )
-}
+  const [title, setTitle] = useState(true);
+  const changeTitle = () => {
+    // set lai phu dinh cuar title
+    setTitle(!title);
+    // true => false
+    // fasle => true
+    const 
+const changeText =
+  };
+  return (
+    <div>
+      <footer>
+        <div className="info-footer">
+          <h6 className="title-ft" onClick={() => changeTitle()}>
+            {title ? 'Quote of the Day' : 'We are hero'}
+          </h6>
+          <h6>---***---</h6>
+          <p className="info-ft">
+            "We are like butterflies who flutter for a day and think it is
+            forever."
+          </p>
+          <p className="author-ft">Carl Sagan</p>
+        </div>
+      </footer>
+    </div>
+  );
+};
